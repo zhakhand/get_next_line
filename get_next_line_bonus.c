b07d8a412366150420b0bd8dc13674ce49f70b4c
Sanchez Jaxon <kaisarulydz@gmail.com>
2024-07-22 00:23:50 +0200

    don't forget headers for bonuses

---
+++ get_next_line_bonus.c
@@ -0,0 +1,123 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzhakhan <dzhakhan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/20 11:59:30 by dzhakhan          #+#    #+#             */
+/*   Updated: 2024/07/20 12:33:28 by dzhakhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strdup(char *s)
+{
+	char	*d;
+	int		i;
+
+	i = 0;
+	d = (char *)malloc(ft_strlen(s) + 1);
+	if (!d)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		d[i] = s[i];
+		i++;
+	}
+	d[i] = '\0';
+	return (d);
+}
+
+static char	*do_other_stuff(char **r)
+{
+	char	*l;
+
+	l = NULL;
+	if (ft_strlen(*r) > 0)
+		l = ft_strdup(*r);
+	ft_free(r);
+	*r = NULL;
+	if (!l)
+		return (NULL);
+	return (l);
+}
+
+static char	*do_something_different(char *n_l_p, char **r)
+{
+	int		l_l;
+	char	*n_r;
+	char	*l;
+
+	l_l = (n_l_p - *r) + 1;
+	l = malloc(l_l + 1);
+	if (!l)
+		return (ft_free(r), NULL);
+	l = ft_strncpy(l, *r, l_l);
+	l[l_l] = '\0';
+	n_r = ft_strdup(n_l_p + 1);
+	ft_free(r);
+	if (!n_r)
+		return (ft_free(&l), NULL);
+	*r = n_r;
+	return (l);
+}
+
+static char	*do_something(int fd, int *b_r, char *r)
+{
+	char	*t;
+	char	*b;
+
+	b = malloc(BUFFER_SIZE + 1);
+	if (!b)
+		return (ft_free(&r), NULL);
+	*b_r = read(fd, b, BUFFER_SIZE);
+	if (*b_r < 0)
+		return (ft_free(&r), ft_free(&b), NULL);
+	b[*b_r] = '\0';
+	if (r)
+	{
+		t = malloc(ft_strlen(r) + *b_r + 1);
+		if (!t)
+			return (ft_free(&r), ft_free(&b), NULL);
+		t = ft_strncpy(t, r, ft_strlen(r) + 1);
+		t = ft_strcat(t, b);
+		ft_free(&b);
+		ft_free(&r);
+		r = t;
+		return (r);
+	}
+	r = ft_strdup(b);
+	ft_free(&b);
+	return (r);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*r[128];
+	char		*l;
+	int			b_r;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	while (1)
+	{
+		r[fd] = do_something(fd, &b_r, r[fd]);
+		if (!r[fd])
+			return (NULL);
+		l = ft_strchr(r[fd], '\n');
+		if (l)
+		{
+			l = do_something_different(l, &r[fd]);
+			if (!l)
+				return (NULL);
+			return (l);
+		}
+		if (b_r < BUFFER_SIZE)
+		{
+			l = do_other_stuff(&r[fd]);
+			return (l);
+		}
+	}
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzhakhan <dzhakhan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/20 11:59:30 by dzhakhan          #+#    #+#             */
+/*   Updated: 2024/07/20 12:33:28 by dzhakhan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <sys/stat.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strdup(char *s);
+char	*ft_strncpy(char *d, char *s, int l);
+char	*ft_strcat(char *d, char *s);
+char	*ft_strchr(char	*s, char c);
+int		ft_strlen(char *s);
+void	ft_free(char **a);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 21
+# endif
+
+#endif
